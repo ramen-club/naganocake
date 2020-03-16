5017d3db5af4e2d3ed1c768b0811805b7be3c858
Haruka W <nekonoko1995@gmail.com>
2020-03-16 17:14:17 +0900

    [Add]会員新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
+
+  resources :items, only: [:index, :show]
+  get '/items/top', to: 'items#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
