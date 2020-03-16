359626ebaa92c2e51af27276dcc3f5cbc28fffde
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-16 13:53:33 +0900

    [Add]delivers,orders,orderz-details

--- db/migrate/20200316032118_create_orders.rb
+++ db/migrate/20200316032118_create_orders.rb
@@ -2,6 +2,15 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
 
+      t.integer "customer_id"
+      t.integer "payment_method"
+      t.integer "payment"
+      t.integer "postage"
+      t.string  "name"
+      t.string  "postal_code"
+      t.string  "street_addres"
+      t.integer "order_status"
+
       t.timestamps
     end
   end
--- db/migrate/20200316032122_create_order_details.rb
+++ db/migrate/20200316032122_create_order_details.rb
@@ -2,6 +2,12 @@ class CreateOrderDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :order_details do |t|
 
+      t.integer "order_id"
+      t.integer "item_id"
+      t.integer "count"
+      t.integer "order_amount"
+      t.integer "production_status"
+
       t.timestamps
     end
   end
