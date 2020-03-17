39d345319921b001ae2147848336fd0824e207e3
Haruka W <nekonoko1995@gmail.com>
2020-03-17 15:10:03 +0900

    [Add]会員情報編集機能を実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,17 @@
 class CustomersController < ApplicationController
 
-  before_action :screen_customer, only: [:edit, :update]
+  before_action :screen_customer, only: [:show, :edit, :update]
 
   def show
   	@customer = Customer.find(params[:id])
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def update
+  	@customer = Customer.find(params[:id])
   end
 
   private
@@ -17,8 +19,8 @@ class CustomersController < ApplicationController
        params.require(:customer).permit(:family_name, :first_name, :family_kana, :first_kana, :email, :postal_code, :street_address, :tel_number)
     end
 
-    def screen_user
-      unless params[:id].to_i == current_cuctomer.id
+    def screen_customer
+      unless params[:id].to_i == current_customer.id
         redirect_to customer_path(current_customer)
       end
     end
