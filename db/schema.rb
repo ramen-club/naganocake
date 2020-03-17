27f393862df1ef690269f68d9224b87941e69cfc
平良海渡 <tairakaito0824@gmail.com>
2020-03-17 17:43:56 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_074707) do
+ActiveRecord::Schema.define(version: 2020_03_17_070257) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
