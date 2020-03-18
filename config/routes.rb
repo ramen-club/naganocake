5b05870ecedfa0344a1a52693f3102c80262ad1b
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-18 18:30:21 +0900

    [Fix]routeがgenre追加によって増えたところを消去

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   
-  namespace :admin do
-    get 'genres/new'
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-    get 'genres/delete'
-  end
   devise_for :admins
   devise_for :customers
 
