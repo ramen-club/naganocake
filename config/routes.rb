a2f8fed75752cf4e6141ad848db1922641e3f4a0
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-18 13:29:28 +0900

    [Add]Adminにresourcesでgenreを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :orders, only: [:update]
+    resources :genres
   end
-
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
