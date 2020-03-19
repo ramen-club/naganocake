1829bca4c203fc21d1066c85cdde07c87ab4f9a8
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-19 14:48:13 +0900

    [Add]genre,70%

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,23 +7,29 @@ class Admin::GenresController < ApplicationController
   def index
     @genres = Genre.all
     @genre = Genre.new
+    # if @genres.is_active == true
+    #    "有効"
+    # else
+    #   "無効"
+    # end
   end
 
   def create
-    @genre = Genres.new(genre_params)
-    @genre.admin_id = current_user.id
+    @genre = Genre.new(genre_params)
+    # @genre.admin_id = current_admin.id
     @genre.save
     redirect_back(fallback_location: root_path)
   end
 
   def edit
-    @genre = Genres.find(params[:id])
+    @genre = Genre.find(params[:id])
+
   end
 
   def update
-    @genre = Genres.find(params[:id])
-    @genre.update
-    redirect_to admin_genre_path
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   # 間違ったgenreを生成したときのために作成(ワークフレームには記述なし)
@@ -35,6 +41,6 @@ class Admin::GenresController < ApplicationController
 
 private
   def genre_params
-    params_require(:genre).permit(:name, :is_active)
+    params.require(:genre).permit(:name, :is_active)
   end
 end
