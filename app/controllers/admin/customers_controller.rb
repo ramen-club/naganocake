81cca678cfc32a961afadde88ddbe3e695507032
Haruka W <nekonoko1995@gmail.com>
2020-03-21 19:15:50 +0900

    [Add]管理者側の会員一覧ページにページャを実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,7 +3,7 @@ class Admin::CustomersController < ApplicationController
 	before_action :admin_admin
 
 	def index
-	  @customers = Customer.all
+	  @customers = Customer.page(params[:page]).reverse_order
 	end
 
 	def show
