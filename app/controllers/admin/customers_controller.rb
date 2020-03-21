5442934f3ace0b34ad8f045df15c65a9a628a04f
Haruka W <nekonoko1995@gmail.com>
2020-03-21 19:01:14 +0900

    [Add]管理者側の会員編集機能の実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -7,14 +7,26 @@ class Admin::CustomersController < ApplicationController
 	end
 
 	def show
+	  @customer = Customer.find(params[:id])
 	end
 
 	def edit
+	  @customer = Customer.find(params[:id])
 	end
+
+	def update
+	  @customer = Customer.find(params[:id])
+  	  @customer.update(customer_params)
+  	  redirect_to admin_customer_path(@customer.id)
+  	end
 
 	private
 	  def admin_admin
 	  	redirect_to(root_url) unless admin_signed_in?
 	  end
+
+      def customer_params
+       params.require(:customer).permit(:family_name, :first_name, :family_kana, :first_kana, :email, :postal_code, :street_address, :tel_number, :is_active)
+      end
 
 end
