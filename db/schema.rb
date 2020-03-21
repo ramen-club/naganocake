7e79e238734f83f4760c1084cf606c34d9f8e2d1
平良海渡 <tairakaito0824@gmail.com>
2020-03-21 16:40:26 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_03_21_053348) do
 
-ActiveRecord::Schema.define(version: 2020_03_19_085334) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
