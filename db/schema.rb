0e73a7ba01cc5be86d4462c681140d6901868206
Haruka W <nekonoko1995@gmail.com>
2020-03-21 15:56:40 +0900

    [Update]会員ステータスの退会処理を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_19_085334) do
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_085334) do
     t.string "postal_code"
     t.string "street_address"
     t.string "tel_number"
-    t.boolean "is_active", default: true
+    t.integer "is_active", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# coding: utf-8
+
+# 管理者の登録情報
 Admin.create!(email: "admin@example.jp",
              password:  "ramenclub",
              password_confirmation: "ramenclub",
              admin_flg: true)
+
+# 顧客のテストデータ
+Customer.create!(
+	email: "hoge@example.com",
+	family_name: "山田",
+	first_name: "花子",
+	family_kana: "ヤマダ",
+	first_kana: "ハナコ",
+	postal_code: "1500041",
+	street_address: "東京都渋谷区神南1丁目19-11",
+	tel_number: "0368694700",
+	password: "123456")
