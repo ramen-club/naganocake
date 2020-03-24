ed68eb64dfa49ab70b7e98a89aa4b8294496ff4f
平良海渡 <tairakaito0824@gmail.com>
2020-03-24 19:04:49 +0900

    商品がカートにセーブされない

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,56 +3,37 @@ class CartsController < ApplicationController
     def show
     end
 
-    def add_item
-        if  @cart_item.blank?
-        @cart_item = current_cart.cart_items.build(Cart_id: params[:cart_items_id])
-    end
-
-    @cart_item.quantity += params[:quantity].to_i
-    @cart_item.save
-    redirect_to current_cart
-    end
-
     def index
        @carts = current_customer.cart
-       # @cart = Cart.find
        @cart_items = current_customer.relations
+       @cart_item = CartItem.new
     end
 
     def create
-        current_customers.cart(params[:item_id])
-        redirect_to action: :sow
+        @cart_item = CartItem.new(params[:cart_item_id])
+        @cart_item.save
+        redirect_to carts_path
     end
 
-     def update_item
+    def update_item
         @cart_item = CartItem.find(params[:cart_item_id])
         @cart_item.count = params[:cart_item][:count]
         @cart_item.update(cart_item_params)
         redirect_to carts_path
-        # @cart_item = Cart.find(params[:id])
-        # @cart.update(cart_params)
-        # redirect_back(fallback_location: root_path)
-        # @cart_item.update(quantity: params[:quantity].to_i)
-        # redirect_to current_cart
     end
         # 商品消去
     def delete_item
-        # @cart_item = current_cart_item
         cart_item = CartItem.find(params[:cart_item_id])
         cart_item.destroy
         redirect_to carts_path
     end
         # カート内消去
     def destroy
-        # @cart = current_cart
         cart = Cart.find(params[:id])
         cart.cart_items.each do|cart_item|
         cart_item.destroy
         end
         redirect_to carts_path
-        # else
-        # redirect_back(fallback_location: carts_path)
-        # end
     end
 
     private
