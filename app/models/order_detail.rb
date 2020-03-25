7b1eff5b2aaf6546f3f72429a07e4ced30f40d0f
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-26 01:22:55 +0900

    [add] admin_order

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,6 +3,17 @@ class OrderDetail < ApplicationRecord
 	belongs_to :order
 	belongs_to :item
 
-	
+	enum production_status: {
+        着手不可: 0,
+        製作待ち: 1,
+        製作中: 2,
+        製作完了: 3
+    }
 
+    def total_price
+        price = 0
+        order_details.each do |order_detail|
+          price +=  order_detail.item.price * order_detail.count
+        end
+    end
 end
