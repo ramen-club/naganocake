4803e649587400df73e393473b0d99cb266c44b0
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-03-26 13:43:32 +0900

    [Fix]admin/order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,24 +3,24 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :if_not_admin
   before_action :set_order_detail, only: [:edit]
 
-  def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-    @total_price = @order_details.sum(:order_amount)
-    # binding.pry
-    # @production_status = @order_details.production_status.find(params[:id])
-  end
+  # def show
+  #   @order = Order.find(params[:id])
+  #   @order_details = @order.order_details
+  #   @total_price = @order_details.sum(:order_amount)
+  #   # binding.pry
+  #   # @production_status = @order_details.production_status.find(params[:id])
+  # end
   
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-    redirect_to admin_order_details_path
+    redirect_back(fallback_location: root_path)
   end
   
-  def index
-    @order_details = OrderDetail.page(params[:page]).reverse_order
-    @count_total = OrderDetail.sum(:count)
-  end
+  # def index
+  #   @order_details = OrderDetail.page(params[:page]).reverse_order
+  #   @count_total = OrderDetail.sum(:count)
+  # end
 
   private
   def order_detail_params
