314dc1d4c852193beeaf1ed71196a066a2b8e712
Haruka W <nekonoko1995@gmail.com>
2020-03-29 19:34:42 +0900

    [Add]検索機能を実装

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,30 @@
 class SearchController < ApplicationController
 
+	before_action :authenticate_admin!
+
 	def search
-		@items = Item.search(params[:search])
+		@model = params["search"]["model"]
+		@content = params["search"]["content"]
+		@records = search_for(@model, @content).page(params[:page]).reverse_order
 	end
 
 
 	private
 
-	def search(search)
-		if search
-			Item.where(['name LIKE ?', "%#{search}%"])
-		else
-			Item.all
+	def search_for(model,content)
+		if model == 'item'
+			Item.where(name: content)
+		else model == 'customer'
+			Customer.where({family_name: content}, {first_name: content})
 		end
 	end
+
+	# def search(search)
+	# 	if search
+	# 		Item.where(['name LIKE ?', "%#{search}%"])
+	# 	else
+	# 		Item.all
+	# 	end
+	# end
 
 end
